972418b958e5c4efcc8b911780f756404226aa5d
Rico Prodan <rico.prodan@gmail.com>
2023-09-12 15:05:33 +0200

    add even more things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ChatMessage} from "./entity/chat-message";
-import {formatDate} from "@angular/common";
 import {RxStompService} from "./websocket/rx-stomp.service";
 import {Subscription} from "rxjs";
 import {Message} from "@stomp/stompjs";
+import {MessageType} from "./enumeration/message-type";
 
 @Component({
   selector: 'app-root',
@@ -22,6 +22,29 @@ export class AppComponent implements OnInit, OnDestroy{
   constructor(private rxStompService: RxStompService) {
   }
 
+  sendMessage() {
+    let msg: ChatMessage = {
+      user: this.user,
+      content: this.message,
+      type: MessageType.MESSAGE,
+      date: new Date()
+    }
+
+    this.rxStompService.publish({ destination: '/app/chat.sendMessage', body: JSON.stringify(msg) });
+    this.message = "";
+  }
+
+  start() {
+    this.isUsernameSet = true
+  }
+
+  formatDate(date: Date): string {
+    let formattedDate = new Date(date)
+
+    const hour   = ("0" + formattedDate.getHours()).slice(-2);
+    const minute = ("0" + formattedDate.getMinutes()).slice(-2);
+    return hour + ":" + minute;
+  }
   ngOnInit() {
     this.topicSubscription = this.rxStompService
       .watch('/topic/chat')
@@ -33,6 +56,4 @@ export class AppComponent implements OnInit, OnDestroy{
   ngOnDestroy() {
     this.topicSubscription.unsubscribe();
   }
-
-  protected readonly formatDate = formatDate;
 }
